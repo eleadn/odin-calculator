62d39a12dcb532ff2513a2a1d12ea0e91b35b0ae
eleadn <eleano.dn@gmail.com>
2025-04-20 16:53:39 +0200

    Add logic for buttons

--- script.js
+++ script.js
@@ -50,8 +50,63 @@ function Operation(leftOperand = null, operator = null, rightOperand = null)
     return obj;
 }
 
-let operation = new Operation(3, "+", 5);
+function onNumberButtonClick(numberButton, operation, display)
+{
+    if (display.textContent.length < 9)
+    {
+        display.textContent += numberButton.textContent;
+    }
 
-operate(operation);
+    if (operation.operator === null)
+    {
+        operation.leftOperand = Number.parseInt(display.textContent);
+    }
+    else
+    {
+        operation.rightOperand = Number.parseInt(display.textContent);
+    }
+}
 
-console.log(operation.result);
+function onClearButtonClick(operation, display, preview)
+{
+    operation.leftOperand = null;
+    operation.operator = null;
+    operation.rightOperand = null;
+    operation.result = null;
+    display.textContent = "";
+    preview.textContent = "";
+}
+
+function onOperatorClick(operatorButton, operation, display, preview)
+{
+    if (operation.leftOperand === null)
+    {
+        return;
+    }
+
+    operation.operator = operatorButton.textContent;
+    preview.textContent = `${operation.leftOperand}${operation.operator}`;
+    display.textContent = "";
+}
+
+function onSubmitClick(operation, display, preview)
+{
+    operate(operation);
+    preview.textContent = `${operation.leftOperand}${operation.operator}${operation.rightOperand}=`;
+    display.textContent = operation.result;
+}
+
+const currentDisplay = document.querySelector(".current");
+const previewDisplay = document.querySelector(".preview");
+const clearButton = document.querySelector(".clear");
+const submitButton = document.querySelector(".submit");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+let operation = new Operation();
+
+numberButtons.forEach(numberButton => numberButton.addEventListener("click",
+    context => onNumberButtonClick(context.target, operation, currentDisplay)));
+clearButton.addEventListener("click", () => onClearButtonClick(operation, currentDisplay, previewDisplay));
+operatorButtons.forEach(operatorButton => operatorButton.addEventListener("click",
+    context => onOperatorClick(context.target, operation, currentDisplay, previewDisplay)));
+submitButton.addEventListener("click", () => onSubmitClick(operation, currentDisplay, previewDisplay));
